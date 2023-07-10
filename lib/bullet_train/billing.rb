16518023ec9951c0cfac7fa26a8b4d28b431bb5b
Kasper Timm Hansen <kaspth@gmail.com>
2023-07-10 17:24:08 +0200

    Extract remaining Billing extensions to our repo

--- lib/bullet_train/billing.rb
+++ lib/bullet_train/billing.rb
@@ -3,6 +3,29 @@ require "bullet_train/billing/engine"
 
 module BulletTrain
   module Billing
+    module Records
+      module Base
+        extend ActiveSupport::Concern
+
+        included do
+          # By default, any model in a collection is considered active for billing purposes.
+          # This can be overloaded in the child model class to specify more specific criteria for billing.
+          # See `Memberships::Base` below for an example.
+          scope :billable, -> { order("TRUE") } # TODO: Replace empty condition with just `all` to return the current relation.
+        end
+      end
+    end
+
+    module Memberships
+      module Base
+        extend ActiveSupport::Concern
+
+        included do
+          scope :billable, -> { current_and_invited }
+        end
+      end
+    end
+
     module Teams
       module Base
         extend ActiveSupport::Concern
@@ -30,4 +53,7 @@ def freemium_enabled?
   Billing::Product.find_by(id: "free").present?
 end
 
+ActiveSupport.on_load(:bullet_train_records_base) { include BulletTrain::Billing::Records::Base }
+ActiveSupport.on_load(:bullet_train_memberships_base) { include BulletTrain::Billing::Memberships::Base }
 ActiveSupport.on_load(:bullet_train_teams_base) { include BulletTrain::Billing::Teams::Base }
+ActiveSupport.on_load(:bullet_train_account_controllers_base) { include Billing::ControllerSupport }
